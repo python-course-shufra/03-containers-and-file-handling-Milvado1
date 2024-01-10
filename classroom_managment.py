8886b53d08c1230beba6886a9e88eb6c703af2f9
Rachel <rotenberg100@gmail.com>
2024-01-10 23:57:29 +0200

    fix bug lower

--- classroom_managment.py
+++ classroom_managment.py
@@ -43,7 +43,7 @@ def add_student(name, email=None):
     'grade': initialize with empty list
     """
     if email == None:
-        email = name+'@example.com'
+        email = name.lower()+'@example.com'
 
     student = {}
     student['name'] = name
